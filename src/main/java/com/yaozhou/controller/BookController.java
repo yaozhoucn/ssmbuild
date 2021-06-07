dcf1dd263f400d0dabca1e07a494861cd97c6b8d
yaozhoucn <wangxiao_h@126.com>
2021-06-07 11:24:28 +0800

    sping-mvc

--- src/main/java/com/yaozhou/controller/BookController.java
+++ src/main/java/com/yaozhou/controller/BookController.java
@@ -1,20 +1,32 @@
 package com.yaozhou.controller;
 
+import com.yaozhou.pojo.Books;
 import com.yaozhou.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by WXHang on HANG at 2021/6/7 10:15
  * Desc：
  */
 @Controller
-@RequestMapping
+@RequestMapping("/book")
 public class BookController {
     //controller调用service层
     @Autowired
     @Qualifier("BooksServiceImp")
     private BookService bookService;
+    //查询所有书籍，并且返回一个书籍展示页面
+    @RequestMapping("/allBook")
+    public String list(Model model){
+        List<Books> list = bookService.querAllBook();
+        model.addAttribute("list",list);
+        //返回前端展示
+        return "allBook";
+    }
 }
